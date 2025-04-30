2388be20d80af832a495136c0e64cf33ca8ef9ef
sandhm1 <manjotssandhu1@gmail.com>
2025-04-30 12:13:48 -0700

    updates to utils

--- utils.py
+++ utils.py
@@ -14,10 +14,10 @@ def iou(target, pred, format="corners"):
     else:
         raise Exception("format not supported")
 
-    x1 = max(box1_x1, box2_x1)
-    y1 = max(box1_y1, box2_y1)
-    x2 = min(box1_x2, box2_x2)
-    y2 = min(box1_y2, box2_y2)
+    x1 = torch.max(box1_x1, box2_x1)
+    y1 = torch.max(box1_y1, box2_y1)
+    x2 = torch.min(box1_x2, box2_x2)
+    y2 = torch.min(box1_y2, box2_y2)
 
     intersection = (x2 - x1).clamp(0) * (y2 - y1).clamp(0)
 
@@ -27,6 +27,51 @@ def iou(target, pred, format="corners"):
 
     return intersection / (union + 1e-6)
 
+def iou_matrix(boxes):
+    x1 = boxes[:, 0].unsqueeze(1)
+    y1 = boxes[:, 1].unsqueeze(1)
+    x2 = boxes[:, 2].unsqueeze(1)
+    y2 = boxes[:, 3].unsqueeze(1)
+
+    xx1 = torch.max(x1, x1.T)
+    yy1 = torch.max(y1, y1.T)
+    xx2 = torch.min(x2, x2.T)
+    yy2 = torch.min(y2, y2.T)
+
+    intersection = (xx2 - xx1).clamp(0) * (yy2 - yy1).clamp(0)
+
+    area = (x2 - x1).clamp(0) * (y2 - y1).clamp(0)
+    union = area + area.T - intersection
+
+    iou = intersection / (union + 1e-6)
+    iou.fill_diagonal_(0)
+    iou = torch.max(iou, dim=1)
+
+    return iou
+
 # Non max suppression
-def nms(dets, thresh):
-    
+def nms(pred, conf_thresh=0.5, iou_thresh=0.5, format="corners"):
+    # pred: [[classNum, prediction, x1, y1, x2, y2], [...], [...]]
+    assert type(pred) == list
+    pred = (p for p in pred if p[1] > conf_thresh)
+    pred = sorted(pred, key=lambda x: x[1], reverse=True)
+
+    boxes = []
+
+    for box in pred:
+
+    # found = {}
+    # for box in pred:
+    #     if box[0] not in found:
+    #         found[box[0]] = []
+    #         found[box[0]].append(box)
+    #     else:
+    #         suppress = False
+    #         for currBox in found[box[0]]:
+    #             if iou(torch.tensor(currBox[2:]), torch.tensor(box[2:]), format) > iou_thresh:
+    #                 suppress = True
+    #                 break
+    #         if not suppress:
+    #             found[box[0]].append(box)
+
+    return [box for boxes in found.values() for box in boxes]
