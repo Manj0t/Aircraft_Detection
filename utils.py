28afe7b642de1914fea7144e4f9066a0604cb2c5
Manj0t <manjotssandhu1@gmail.com>
2025-04-30 10:51:20 -0700

    Implemented non max suppression function

--- utils.py
+++ utils.py
@@ -14,10 +14,10 @@ def iou(target, pred, format="corners"):
     else:
         raise Exception("format not supported")
 
-    x1 = max(box1_x1, box2_x1)
-    y1 = max(box1_y1, box2_y1)
-    x2 = min(box1_x2, box2_x2)
-    y2 = min(box1_y2, box2_y2)
+    x1 = torch.max(box1_x1, box2_x1)
+    y1 = torch.max(box1_y1, box2_y1)
+    x2 = torch.min(box1_x2, box2_x2)
+    y2 = torch.min(box1_y2, box2_y2)
 
     intersection = (x2 - x1).clamp(0) * (y2 - y1).clamp(0)
 
@@ -28,5 +28,23 @@ def iou(target, pred, format="corners"):
     return intersection / (union + 1e-6)
 
 # Non max suppression
-def nms(dets, thresh):
-    
+def nms(pred, conf_thresh=0.5, iou_thresh=0.5, format="corners"):
+    # pred: [[classNum, prediction, x1, y1, x2, y2], [...], [...]]
+    assert type(pred) == list
+    pred = sorted((p for p in pred if p[1] > conf_thresh), key=lambda x: x[1], reverse=True)
+
+    found = {}
+    for box in pred:
+        if box[0] not in found:
+            found[box[0]] = []
+            found[box[0]].append(box)
+        else:
+            suppress = False
+            for currBox in found[box[0]]:
+                if iou(torch.tensor(currBox[2:]), torch.tensor(box[2:]), format) > iou_thresh:
+                    suppress = True
+                    break
+            if not suppress:
+                found[box[0]].append(box)
+
+    return [box for boxes in found.values() for box in boxes]
