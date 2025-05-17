fed93237afa14e88ae640fd32cc0d357ba454ef5
Manj0t <manjotssandhu1@gmail.com>
2025-05-16 19:57:50 -0700

    Completed model

--- model.py
+++ model.py
@@ -65,8 +65,74 @@ class ResidualBlock(nn.Module):
         return x
 
 class ScalePrediction(nn.Module):
-    def __init__(self):
+    def __init__(self, in_channels, num_classes):
+        super().__init__()
+        self.pred = nn.Sequential(
+            CNNBlock(in_channels, 2*in_channels, kernel_size=3, padding=1),
+            CNNBlock(2 * in_channels, (num_classes + 5) * 3, bn_act=False, kernel_size=1),
+        )
+        self.num_classes = num_classes
 
+    def forward(self, x):
+        x = self.pred(x)
+        x = x.reshape(x.shape[0], 3, self.num_classes + 5, x.shape[2], x.shape[3])
+        x = x.permute(0, 1, 3, 4, 2)
+        return x
 
 class YOLOv3(nn.Module):
-    def __init__(self):
+    def __init__(self, in_channels=3, num_classes=20):
+        super().__init__()
+        self.num_classes = num_classes
+        self.in_channels = in_channels
+        self.layers = self._create_conve_layers()
+
+    def forward(self, x):
+        outputs = []
+        route_connections = []
+
+        for layer in self.layers:
+            if isinstance(layer, ScalePrediction):
+                outputs.append(layer(x))
+                continue
+
+            x = layer(x)
+
+            if isinstance(layer, ResidualBlock) and layer.num_repeats == 8:
+                route_connections.append(x)
+
+            elif isinstance(layer, nn.Upsample):
+                x = torch.concat((x, route_connections[-1]), dim=1)
+                route_connections.pop()
+
+        return outputs
+
+    def _create_conve_layers(self):
+        layers = nn.ModuleList()
+        in_channels = self.in_channels
+
+        for module in config:
+            if isinstance(module, tuple):
+                out_channels, kernel_size, stride = module
+                layers.append(
+                    CNNBlock(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=1 if kernel_size == 3 else 0)
+                )
+                in_channels = out_channels
+            elif isinstance(module, list):
+                num_repeats = module[1]
+                layers.append(
+                    ResidualBlock(in_channels, num_repeats=num_repeats)
+                )
+            elif module == "S":
+                layers += [
+                    ResidualBlock(in_channels, use_residual=False, num_repeats=1),
+                    CNNBlock(in_channels, in_channels//2, kernel_size=1),
+                    ScalePrediction(in_channels//2, self.num_classes),
+                ]
+                in_channels = in_channels//2
+            elif module == "U":
+                layers.append(
+                    nn.Upsample(scale_factor=2)
+                )
+                in_channels = in_channels * 3
+
+        return layers
